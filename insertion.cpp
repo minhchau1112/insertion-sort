22b06301bd598967c2fe33adb6f7df71719820b8
Trần Ngọc Minh Châu <111561604+minhchau1112@users.noreply.github.com>
2022-08-19 15:54:07 +0700

    Add files via upload

--- insertion.cpp
+++ insertion.cpp
@@ -5,55 +5,7 @@ ai +1 vào trình tự đó sao cho nó vẫn có trật tự.
 
 #include<iostream>
 using namespace std;
-void flash_sort(int a[], int n) {
-	if (n <= 1) return;
-	int m = floor(n * 0.45);
-	// int m = n;
-	for (int i = 0; i < m; ++i)
-		__L[i] = 0;
-	int Mx = a[0], Mn = a[0];
-	for (int i = 1; i < n; ++i) {
-		if (Mx < a[i]) Mx = a[i];
-		if (Mn > a[i]) Mn = a[i];
-	}
-	if (Mx == Mn)
-		return;
-	for (int i = 0; i < n; ++i) {
-		int k = floor(((m - 1) * (a[i] - Mn)) / (Mx - Mn));
-		++__L[k];
-	}
-	for (int i = 1; i < m; ++i)
-		__L[i] += __L[i - 1];
-	//step 2
-	int count = 0;
-	int i = 0;
-	while (count < n) {
-		int k = floor(((m - 1) * (a[i] - Mn)) / (Mx - Mn));
-		while (i >= __L[k]) {
-			++i;
-			k = floor(((m - 1) * (a[i] - Mn)) / (Mx - Mn));
-		}
-		int z = a[i];
-		while (i != __L[k]) {
-			k = floor(((m - 1) * (z - Mn)) / (Mx - Mn));
-			int y = a[__L[k] - 1];
-			a[--__L[k]] = z;
-			z = y;
-			++count;
-		}
-	}
-	//step 3
 
-	for (int k = 1; k < m; ++k) {
-		for (int i = __L[k] - 2; i >= __L[k - 1]; --i)
-			if (a[i] > a[i + 1]) {
-				int t = a[i], j = i;
-				while (t > a[j + 1]) { a[j] = a[j + 1]; ++j; }
-				a[j] = t;
-			}
-	}
-
-}
 void InsertionSort(int* a, int n) {
 	for (int i = 1; i < n; i++) {
 		int x = a[i];
